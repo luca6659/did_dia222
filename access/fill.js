a06522a10064f7419b04f0446f6cc95c040c41b6
luca6659 <111491618+luca6659@users.noreply.github.com>
2026-03-26 19:05:55 +0300

    Update fill.js

--- access/fill.js
+++ access/fill.js
@@ -1,3 +1,24 @@
+// Переменные с данными пользователя
+var isWorking = true;
+var isRightsEnabled = true;  // если права не нужны, поставьте false
+var isCovidCertificateEnabled = true;  // если COVID-сертификат не нужен, поставьте false
+
+var fullname = "Бабаков Лука Сергеевич";
+var name = "Лука";
+var birthdate = "05.09.2011";
+var passport_id = "520471839";
+var kpp_id = "05098251420";
+
+// Данные для водительских прав (если isRightsEnabled = true)
+var rights_valid_until = "01.01.2022";
+var rights_categories = "A, B";
+var rights_tsc = "ТСЦ 8631";
+var rights_id = "KBE968639";
+
+// Данные для COVID-сертификата (если isCovidCertificateEnabled = true)
+var covid_valid_until = "11.03.2022";
+var covid_certificate_id = "URN:UVCI:01:UA:0E55669376876888B9E2520C4F88930";
+
 if(isWorking) {
 	for(el of document.getElementsByClassName("birthdate")){
 		el.innerHTML = "Дата народження: " + birthdate;
